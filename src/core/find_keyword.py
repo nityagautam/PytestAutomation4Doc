89f68a8858dbc57ff9198826ab89fda4c2634cca
Ashutosh Mishra <nityanarayan44@live.com>
2025-06-06 22:27:09 +0530

    enhanced

--- src/core/find_keyword.py
+++ src/core/find_keyword.py
@@ -4,6 +4,8 @@
 import re
 from bs4 import BeautifulSoup
 
+from src.utilities.utilities import Utilities
+
 
 class FindKeyword:
     def __init__(self, src_file: str, keywords_file: str, file_ext: str = "html", verbose: bool = False):
@@ -101,4 +103,59 @@ class FindKeyword:
         return True
     
     def __find_keyword_in_txt(self):
-        return True 
+        # TODO: Does not work fully yet, need to enhance it further
+        """
+        Find keyword in text content.
+        This method reads a text file, checks each line for the presence of keywords,
+        and returns the count of issues found along with the relevant data.
+        Returns:
+            tuple: A tuple containing the count of issues found and a list of dictionaries
+                   with file name, line number, and sentence where the keyword was found.
+        If an error occurs while reading the file, it returns -1 and an empty list.
+        Example:
+            >>> find_keyword = FindKeyword(src_file='example.txt', keywords_file='keywords.txt')
+            >>> issue_count, data = find_keyword.__find_keyword_in_txt()
+            >>> print(issue_count)  # Number of issues found
+            >>> print(data)  # List of dictionaries with file, line, and sentence information
+        Note:
+            - The method assumes that the text file is encoded in UTF-8.
+            - It skips empty lines and only processes non-empty lines.
+            - The line number is set to 1 for the entire text file since it is treated as a single block of text.
+        
+        """
+        issue_counter = 0
+        line_counter = 1
+        file_data = []
+        contents = None
+
+        try:
+            with open(self.src_file, 'r', encoding='utf-8') as file:
+                contents = file.readlines()
+                
+            for content in contents:
+                content = content.strip()
+                
+                # Skip empty lines
+                if not content:
+                    continue
+
+                # Iterate through each keyword and check if it is present in the content
+                for keyword in self.keywords:
+                    
+                    if keyword.lower() in content.lower():
+                        issue_counter += 1
+                        file_data.append({
+                            "file": self.src_file,
+                            "line": 1,  # For text files, we can assume line 1 since it's a single block of text
+                            "sentence": content.strip()
+                        })
+                # Increment the line counter for each line processed
+                line_counter += 1
+
+        except Exception as e:
+            if self.verbose:
+                print(f"Error reading text file {self.src_file}: {e}")
+            return -1, file_data
+        
+        # Return the Collected data
+        return issue_counter, file_data 
